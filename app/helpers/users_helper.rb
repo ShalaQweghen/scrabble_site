6664cfafc74d3c9575088d0727c7e30d4b442640
Serafettin Yilmaz <serafettinyilmaz04@gmail.com>
2018-07-20 21:51:54 +0300

    Refactor game info table to a helper method

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,45 +1,5 @@
-module ApplicationHelper
-  def bootstrap_class_for(flash_type)
-    { success: "alert-success", error: "alert-danger", alert: "alert-warning", notice: "alert-info" }[flash_type] || flash_type.to_s
-  end
-
-  def render_flash_messages
-    content = ""
-
-    flash.each do |msg_type, message|
-      content += content_tag(:div, class: "alert #{bootstrap_class_for(msg_type.to_sym)} fade-in center") do 
-        (message + content_tag(:button, 'x', class: "close small-text", data: { dismiss: 'alert' })).html_safe
-      end
-    end
-
-    content.html_safe
-  end
-
-  def login_helper
-    if current_user
-      (content_tag(:li, class: "nav-item") do
-        link_to "Create", new_game_path, class: "nav-link"
-      end) + 
-      (content_tag(:li, class: "nav-item") do
-        link_to "Join", games_path, class: "nav-link"
-      end) +
-      (content_tag(:li, class: "nav-item") do
-        link_to "Account", user_path(current_user), class: "nav-link"
-      end) + 
-      (content_tag(:li, class: "nav-item") do
-        link_to "Logout", destroy_user_session_path, method: :delete, class: "nav-link"
-      end)
-    else
-      (content_tag(:li, class: "nav-item") do
-        link_to "Register", new_user_registration_path, class: "nav-link"
-      end) + 
-      (content_tag(:li, class: "nav-item") do
-        link_to "Login", new_user_session_path, class: "nav-link"
-      end)
-    end
-  end
-
-  def game_table(game, user_id)
+module UsersHelper
+  def render_game_info_table(game, user_id)
     if game.host_id == user_id
       kind = "hosted"
       outcome = game.host_score > game.part_score ? "won" : "lost"
