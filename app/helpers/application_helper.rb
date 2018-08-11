6d383edcec6ee8ac960261c727f35b1ef37a4b1d
Serafettin Yilmaz <serafettinyilmaz04@gmail.com>
2018-08-11 16:28:42 +0300

    Fix a typo in helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def invitation_link
-    link_to("/games/#invitations", id: "invitation", target: "_blank", class: "btn btn-danger btn-sm mb-1 mr-2 #{" invisible" if !current_user.invited?}") do
-      (fa_icon "envelope") + content_tag(:sup, current_user.times_invited)
+    link_to("/games/#invitations", id: "invitation", target: "_blank", class: "btn btn-danger btn-sm mb-1 mr-2 #{" invisible" if !current_user || !current_user.invited?}") do
+      (fa_icon "envelope") + content_tag(:sup, current_user ? current_user.times_invited : "")
     end
   end
 
